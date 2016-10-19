bcbdaa8da9e6a1e261e2f35317b921d2855f3a7e
Nirmal Singh <nirmal.singh@shadowfax.in>
2016-10-19 13:33:02 +0530

    get working

--- app2.py
+++ app2.py
@@ -1,22 +1,35 @@
-from flask import jsonify
+from flask import jsonify, session, request
 from geoalchemy2.elements import WKTElement
-from geoalchemy2 import func
+from sqlalchemy import func
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
-from database_hanlder import connect_to_db, get_db_session
+from database_handler import connect_to_db, get_db_session
 from data_model import Point
+from werkzeug.security import gen_salt
+from flask_oauthlib.provider import OAuth1Provider
+from shapely import wkb
 
 # Creates a Flask app and reads the settings from a
 # configuration file. We then connect to the database specified
 # in the settings file
 app = Flask(__name__)
 app.config.from_pyfile('app.cfg')#todo
+#todo oauth
+# db = SQLAlchemy(app)
+#
+#
+# def current_user():
+#     if 'id' in session:
+#         uid = session['id']
+#         return User.query.get(uid)
+#     return None
+
+
 engine = connect_to_db()
 
 def get_within_radius(session, lat, lng, radius):
     geom_var = WKTElement('POINT({0} {1})'.format(lng, lat), srid=4326)
-    return session.query(Point).filter(func.ST_DWithin, geom_var, radius).all()
-
+    return session.query(Point).filter(func.ST_DWithin(Point.geom, geom_var, radius)).all()
 
 @app.route('/co_ordinates/', methods=['GET', 'POST'])
 def co_ordinates():
@@ -33,14 +46,16 @@ def co_ordinates():
 
     if request.method == 'GET':
         lat = request.args.get('lat')
-        lng = request.form('long')
-        radius = request.form('radius')
+        lng = request.args.get('long')
+        radius = request.args.get('radius')
         if lat and lng and radius:
             session = get_db_session(engine)
             result = get_within_radius(session, lat, lng, radius)
             final_res = {'data': []}
             for i in result:
-                final_res['data'].append(i.__dict__)
+                point = wkb.loads(bytes(i.geom.data))
+                final_res['data'].append([point.x,point.y])
+            print(final_res)
             return jsonify(final_res)
 
 if __name__ == '__main__':
